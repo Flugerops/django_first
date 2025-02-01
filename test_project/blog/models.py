430674702edfc1b4309371d7c19b1a133a7682fc
Bohdan Savin <bogdan07112009@gmail.com>
2025-02-01 21:23:04 +0200

    second hm done

--- test_project/blog/models.py
+++ test_project/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=240)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+    def __repr__(self):
+        return f"{self.title}"
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+
+    def __repr__(self):
+        return f"{self.content}"
