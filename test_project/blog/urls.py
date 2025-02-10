bd63e7c22dc4741eeb6c03c8ce7f339fd8a241c8
Bohdan Savin <bogdan07112009@gmail.com>
2025-02-10 20:50:13 +0200

    forms done

--- test_project/blog/urls.py
+++ test_project/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home_page, post_page
+from .views import home_page, post_page, create_post
 
 
 app_name = "blog"
@@ -8,4 +8,5 @@ app_name = "blog"
 urlpatterns = [
     path("posts/", home_page, name="home_page"),
     path("posts/<int:post_id>/", post_page, name="post_page"),
+    path("create_post", create_post, name="create_post"),
 ]
--- test_project/blog/views.py
+++ test_project/blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
+from django.contrib.auth.models import User
+
 from .models import Post
 
 
@@ -11,3 +13,21 @@ def home_page(request):
 def post_page(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, "post.html", {"post": post})
+
+
+def create_post(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        author = get_object_or_404(User, id=1)
+        try:
+            post = Post(title=title, content=content, author=author)
+            post.full_clean()
+        except Exception as e:
+            print(e)
+            return HttpResponse("<h1>Bad values</h1>")
+        else:
+            post.save()
+        return redirect("blog:home_page")
+
+    return render(request, "create_post.html")
