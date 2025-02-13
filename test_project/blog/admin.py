075bc1e5a6c09ed03f0133fb4a3af4a915d7b749
Bohdan Savin <bogdan07112009@gmail.com>
2025-02-13 20:16:54 +0200

    admin and many-to-many fix!

--- test_project/blog/admin.py
+++ test_project/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Post, Comment, Tag
+
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin): ...
+
+
+class CommentAdmin(admin.ModelAdmin): ...
+
+
+class TagAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Tag, TagAdmin)
